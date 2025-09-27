5cf386184bb703102c3af7f6e5e5e31ab4a8ea16
ggretagercari <greta.gercari@donbosko-kosova.com>
2025-09-27 12:49:35 +0200

    changes

--- header.php
+++ header.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
-<html <?php language_attributes();?>>
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset');?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Digital School</title>
-    <?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
 </head>
-<body id="top" <?php body_class();?>>
-    
+<body id="top" <?php body_class(); ?>>
+
+
 <header class="site-header">
-<a class="site-title" href="<?php echo esc_url(home_url('/'))?>">
-    <?php bloginfo('name')?>
-</a>
+  <a class="site-title" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <?php bloginfo('name'); ?>
+  </a>
+  <?php if ( get_bloginfo('description') ) : ?>
+    <p class="site-tagline"><?php bloginfo('description'); ?></p>
+  <?php endif; ?>
 
-<?php if(get_bloginfo('description')):?>
-<p class="site-tagline"><?php bloginfo('description') ?></p>
-<?php endif; ?>
 
-</header>
+  </header>
--- index.php
+++ index.php
@@ -2,5 +2,10 @@
 
 <main>
     <h1>Digital School</h1>
-</main>
 
+    <?php if(have_posts()): while (have_posts()): the_post(); ?>
+    <h2><?php the_title(); ?></h2>
+    <p><?php the_content() ?></p>
+    <?php endwhile; endif; ?>
+</main>
+<?php get_footer(); ?>
